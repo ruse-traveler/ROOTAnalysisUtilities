5588030d7e720688fe4f9c1543b4c4301b8b7005
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-10-20 22:51:30 -0400

    Update test macro for compiling

---
+++ test/TestAnalysisUtilities.cxx
@@ -0,0 +1,31 @@
+/// ===========================================================================
+/*! \file   TestAnalysisUtilities.cxx
+ *  \author Derek Anderson
+ *  \date   08.07.2024
+ *
+ *  Unit tests for the ROOTAnalysisUtilities
+ *  library.
+ */
+/// ===========================================================================
+
+#ifndef TestGraphicalUtilities_cxx
+#define TestGraphicalUtilities_cxx
+
+// c++ utilities
+#include <utility>
+// analysis utility
+#include "../include/ROOTAnalysisUtilities.hxx"
+
+
+
+// FIXME it might be more appropriate to just make a ROOT macro
+//   to be compiled...
+TEST_CASE("TestAnalysisUtilities") {
+
+  /* TODO create tests here */
+
+}  // end tests
+
+#endif
+
+// end ========================================================================
